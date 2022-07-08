8cf4323ff5ffc09f25f99ffde149875dba5464f5
Fran Herrero <francisco.herrero@axa-groupsolutions.com>
2022-07-08 22:50:13 +0200

    chore: remove invalid license header

--- packages/bautajs-example/server/resolvers/v1/source/cache-resolver.js
+++ packages/bautajs-example/server/resolvers/v1/source/cache-resolver.js
@@ -1,17 +1,3 @@
-/*
- * Copyright (c) AXA Group Operations Spain S.A.
- *
- * Licensed under the AXA Group Operations Spain S.A. License (the "License");
- * you may not use this file except in compliance with the License.
- * A copy of the License can be found in the LICENSE.TXT file distributed
- * together with this file.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 const { getRequest } = require('@axa/bautajs-express');
 const { pipe, resolver, step } = require('@axa/bautajs-core');
 const { cache } = require('@axa/bautajs-decorator-cache');
