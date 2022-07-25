554be7d5003212f452573b613aa0c77cec8a8ba8
Xavier Redondo <xavier.redondo@axa.com>
2022-07-25 10:00:59 +0200

    fix(core): add suport for promises to tap decorator

--- packages/bautajs-core/src/decorators/__tests__/tap.test.ts
+++ packages/bautajs-core/src/decorators/__tests__/tap.test.ts
@@ -15,4 +15,45 @@ describe('tap decorator', () => {
 
     expect(log).toHaveBeenCalledWith('star wars');
   });
+
+  test('should perform asynchronously the current step action but return the previous step value', async () => {
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+    const log = jest.fn();
+    const tappedPromise = async (name: string) => {
+      await delay(100);
+      return log(name);
+    };
+    const getMovie = step(() => ({ name: 'star wars' }));
+
+    const pipeline = pipe(
+      getMovie,
+      tap(step<{ name: string }, { name: string }>(async ({ name }) => tappedPromise(name)))
+    );
+
+    await expect(pipeline({}, createContext({}), {} as BautaJSInstance)).resolves.toStrictEqual({
+      name: 'star wars'
+    });
+
+    expect(log).toHaveBeenCalledWith('star wars');
+  });
+
+  test('should ignore the error inside tap and return the previous step value', async () => {
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+    const tappedPromise = async (name: string) => {
+      await delay(100);
+
+      return Promise.reject(new Error(`We have an error: ${name}`));
+    };
+    const getMovie = step(() => ({ name: 'star wars' }));
+
+    const pipeline = pipe(
+      getMovie,
+      tap(step<{ name: string }, { name: string }>(async ({ name }) => tappedPromise(name)))
+    );
+
+    await expect(pipeline({}, createContext({}), {} as BautaJSInstance)).resolves.toStrictEqual({
+      name: 'star wars'
+    });
+  });
 });
--- packages/bautajs-core/src/decorators/tap.ts
+++ packages/bautajs-core/src/decorators/tap.ts
@@ -1,7 +1,12 @@
 import { Pipeline } from '../types';
+import { isPromise } from '../utils/is-promise';
+
 /**
  * Decorator that allows to transparently perform actions or side-effects,
  * such as logging and return the previous step result.
+ *
+ * Tap supports promises, but it will ignore any error thrown inside the tapped
+ * promise and will wait for its execution just to ignore its value.
  *
  * @param {Pipeline.StepFunction<TIn, any>} stepFn - The step fn to execute
  * @returns {Pipeline.StepFunction<TIn, TIn>}
@@ -27,8 +32,11 @@ import { Pipeline } from '../types';
  */
 export function tap<TIn>(stepFn: Pipeline.StepFunction<TIn, TIn>): Pipeline.StepFunction<TIn, TIn> {
   return (prev, ctx, bautajs) => {
-    stepFn(prev, ctx, bautajs);
+    const result = stepFn(prev, ctx, bautajs);
 
+    if (isPromise(result)) {
+      return (result as Promise<typeof result>).then(() => prev).catch(() => prev);
+    }
     return prev;
   };
 }
