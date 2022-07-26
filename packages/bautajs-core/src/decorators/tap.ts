941bfa4f653ed0d237bdcaff6acc624ab685d457
Xavier Redondo Bolet <xavier.redondo@axa.com>
2022-07-26 23:57:32 +0200

    feat(core): add support for promises to tap decorator (#28)

--- packages/bautajs-core/src/decorators/tap.ts
+++ packages/bautajs-core/src/decorators/tap.ts
@@ -1,7 +1,12 @@
 import { Pipeline } from '../types';
+import { isPromise } from '../utils/is-promise';
+
 /**
  * Decorator that allows to transparently perform actions or side-effects,
  * such as logging and return the previous step result.
+ *
+ * Tap supports promises, but it will ignore any error thrown inside the tapped
+ * promise and will wait for its execution just to ignore its value.
  *
  * @param {Pipeline.StepFunction<TIn, any>} stepFn - The step fn to execute
  * @returns {Pipeline.StepFunction<TIn, TIn>}
@@ -15,20 +20,27 @@ import { Pipeline } from '../types';
  *  const pipeline = pipe(
  *    randomPreviousPipeline,
  *    tap((prev) => {
- *      console.log(`some intermediate step. Prev is ${prev}`);
- *      // => 'some intermediate step. Prev is I am so random!'
+ *      console.log(`some intermediate step. Prev is "${prev}""`);
+ *      // prints 'some intermediate step. Prev is "I am so random!"'
+ *    }),
+ *    tap(async (prev) => {
+ *      await asyncProcess();
+ *      //'whether asyncProcess resolves or rejects. Prev still will be "I am so random!"'
  *    }),
  *    (prev) => {
  *      console.log(prev);
- *      // print 'I am so random!'
+ *      // prints 'I am so random!'
  *    }
  *  );
  *
  */
 export function tap<TIn>(stepFn: Pipeline.StepFunction<TIn, TIn>): Pipeline.StepFunction<TIn, TIn> {
   return (prev, ctx, bautajs) => {
-    stepFn(prev, ctx, bautajs);
+    const result = stepFn(prev, ctx, bautajs);
 
+    if (isPromise(result)) {
+      return (result as Promise<typeof result>).then(() => prev).catch(() => prev);
+    }
     return prev;
   };
 }
