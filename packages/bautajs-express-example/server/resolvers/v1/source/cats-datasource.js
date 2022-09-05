ce8c600c79d1a7cb026cfb820991e679d52afc8f
Xavier Redondo Bolet <xavier.redondo@axa.com>
2022-09-05 10:51:06 +0200

    fix(bautajs-express-example): rename example express project (#40)
    fix(bautajs-express-example): add custom logger documentation

--- packages/bautajs-express-example/server/resolvers/v1/source/cats-datasource.js
+++ packages/bautajs-express-example/server/resolvers/v1/source/cats-datasource.js
@@ -3,7 +3,7 @@ const { restProvider } = require('@axa/bautajs-datasource-rest');
 // Used to test that an https works
 const catsRestProviderWithHttps = restProvider(client => {
   return client.get('https://cat-fact.herokuapp.com/facts', {
-    rejectUnauthorized: false
+    https: { rejectUnauthorized: false }
   });
 });
 
