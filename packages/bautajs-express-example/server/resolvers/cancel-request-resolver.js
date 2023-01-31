49d57f520bac6b1e29c1a2c0ae65141cbcf98f93
Xavier Redondo <xavier.redondo@axa.com>
2023-01-31 16:24:08 +0100

    fix(bautajs-express-example): add service example
    fix(bautajs-fastify-example): add service example
    
    this is to showcase the cancellation of requests

---
+++ packages/bautajs-express-example/server/resolvers/cancel-request-resolver.js
@@ -0,0 +1,43 @@
+const { resolver, pipe, step } = require('@axa/bautajs-core');
+const { getRequest } = require('@axa/bautajs-express');
+
+const transformResponse = step(response => {
+  return {
+    message: response
+  };
+});
+
+function getNumberFromRequestStep(_prev, ctx) {
+  const req = getRequest(ctx);
+
+  const { number } = req.params;
+
+  return number;
+}
+
+function giveAnswerAfterWaitingWithTimeout() {
+  return step(async (number, ctx) => {
+    const timeout = number * 1000;
+    const promiseAnswer = new Promise(resolve =>
+      setTimeout(() => {
+        resolve(`We have waited for ${number} seconds`);
+      }, timeout)
+    );
+
+    const cancelator = new Promise(resolve =>
+      setTimeout(() => {
+        ctx.token.cancel(); // If this triggers the promise does not resolve but it is cancelled
+        resolve('ended');
+      }, 10000)
+    );
+
+    return Promise.race(await [promiseAnswer, cancelator]);
+  });
+}
+
+module.exports = resolver(operations => {
+  operations.cancelRequest
+    .validateRequest(false)
+    .validateResponse(false)
+    .setup(pipe(getNumberFromRequestStep, giveAnswerAfterWaitingWithTimeout(), transformResponse));
+});
