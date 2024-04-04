5e318af37d1f8f1a68930b7efdd197e8ebf0423d
Xavier Redondo Bolet <xavier.redondo@axa.com>
2024-04-04 16:18:36 +0200

    feat(bautajs-core): update tap behaviour to make it consistent (#139)

--- packages/bautajs-core/src/decorators/tap.ts
+++ packages/bautajs-core/src/decorators/tap.ts
@@ -1,51 +1,225 @@
-import { Pipeline } from '../types';
+import { GenericError, Pipeline, Context, BautaJSInstance } from '../types';
 import { isPromise } from '../utils/is-promise';
+import { compose } from './pipeline';
+
+const defaultErrorHandler: Pipeline.CatchError<GenericError> = e => {
+  throw e;
+};
+
+export function tap<ValueType, ReturnType>(
+  f1: Pipeline.StepFunction<ValueType, ReturnType>
+): Pipeline.PipelineFunction<ValueType, ValueType>;
+
+export function tap<ValueType, ResultValue1, ReturnType>(
+  f1: Pipeline.StepFunction<ValueType, ResultValue1>,
+  f2: Pipeline.StepFunction<ResultValue1, ReturnType>
+): Pipeline.PipelineFunction<ValueType, ValueType>;
+
+export function tap<ValueType, ResultValue1, ResultValue2, ReturnType>(
+  f1: Pipeline.StepFunction<ValueType, ResultValue1>,
+  f2: Pipeline.StepFunction<ResultValue1, ResultValue2>,
+  f3: Pipeline.StepFunction<ResultValue2, ReturnType>
+): Pipeline.PipelineFunction<ValueType, ValueType>;
+
+export function tap<ValueType, ResultValue1, ResultValue2, ResultValue3, ReturnType>(
+  f1: Pipeline.StepFunction<ValueType, ResultValue1>,
+  f2: Pipeline.StepFunction<ResultValue1, ResultValue2>,
+  f3: Pipeline.StepFunction<ResultValue2, ResultValue3>,
+  f4: Pipeline.StepFunction<ResultValue3, ReturnType>
+): Pipeline.PipelineFunction<ValueType, ValueType>;
+
+export function tap<ValueType, ResultValue1, ResultValue2, ResultValue3, ResultValue4, ReturnType>(
+  f1: Pipeline.StepFunction<ValueType, ResultValue1>,
+  f2: Pipeline.StepFunction<ResultValue1, ResultValue2>,
+  f3: Pipeline.StepFunction<ResultValue2, ResultValue3>,
+  f4: Pipeline.StepFunction<ResultValue3, ResultValue4>,
+  f5: Pipeline.StepFunction<ResultValue4, ReturnType>
+): Pipeline.PipelineFunction<ValueType, ValueType>;
+
+export function tap<
+  ValueType,
+  ResultValue1,
+  ResultValue2,
+  ResultValue3,
+  ResultValue4,
+  ResultValue5,
+  ReturnType
+>(
+  f1: Pipeline.StepFunction<ValueType, ResultValue1>,
+  f2: Pipeline.StepFunction<ResultValue1, ResultValue2>,
+  f3: Pipeline.StepFunction<ResultValue2, ResultValue3>,
+  f4: Pipeline.StepFunction<ResultValue3, ResultValue4>,
+  f5: Pipeline.StepFunction<ResultValue4, ResultValue5>,
+  f6: Pipeline.StepFunction<ResultValue5, ReturnType>
+): Pipeline.PipelineFunction<ValueType, ValueType>;
+
+export function tap<
+  ValueType,
+  ResultValue1,
+  ResultValue2,
+  ResultValue3,
+  ResultValue4,
+  ResultValue5,
+  ResultValue6,
+  ReturnType
+>(
+  f1: Pipeline.StepFunction<ValueType, ResultValue1>,
+  f2: Pipeline.StepFunction<ResultValue1, ResultValue2>,
+  f3: Pipeline.StepFunction<ResultValue2, ResultValue3>,
+  f4: Pipeline.StepFunction<ResultValue3, ResultValue4>,
+  f5: Pipeline.StepFunction<ResultValue4, ResultValue5>,
+  f6: Pipeline.StepFunction<ResultValue5, ResultValue6>,
+  f7: Pipeline.StepFunction<ResultValue6, ReturnType>
+): Pipeline.PipelineFunction<ValueType, ValueType>;
+
+export function tap<
+  ValueType,
+  ResultValue1,
+  ResultValue2,
+  ResultValue3,
+  ResultValue4,
+  ResultValue5,
+  ResultValue6,
+  ResultValue7,
+  ReturnType
+>(
+  f1: Pipeline.StepFunction<ValueType, ResultValue1>,
+  f2: Pipeline.StepFunction<ResultValue1, ResultValue2>,
+  f3: Pipeline.StepFunction<ResultValue2, ResultValue3>,
+  f4: Pipeline.StepFunction<ResultValue3, ResultValue4>,
+  f5: Pipeline.StepFunction<ResultValue4, ResultValue5>,
+  f6: Pipeline.StepFunction<ResultValue5, ResultValue6>,
+  f7: Pipeline.StepFunction<ResultValue6, ResultValue7>,
+  f8: Pipeline.StepFunction<ResultValue7, ReturnType>
+): Pipeline.PipelineFunction<ValueType, ValueType>;
+
+export function tap<
+  ValueType,
+  ResultValue1,
+  ResultValue2,
+  ResultValue3,
+  ResultValue4,
+  ResultValue5,
+  ResultValue6,
+  ResultValue7,
+  ResultValue8,
+  ReturnType
+>(
+  f1: Pipeline.StepFunction<ValueType, ResultValue1>,
+  f2: Pipeline.StepFunction<ResultValue1, ResultValue2>,
+  f3: Pipeline.StepFunction<ResultValue2, ResultValue3>,
+  f4: Pipeline.StepFunction<ResultValue3, ResultValue4>,
+  f5: Pipeline.StepFunction<ResultValue4, ResultValue5>,
+  f6: Pipeline.StepFunction<ResultValue5, ResultValue6>,
+  f7: Pipeline.StepFunction<ResultValue6, ResultValue7>,
+  f8: Pipeline.StepFunction<ResultValue7, ResultValue8>,
+  f9: Pipeline.StepFunction<ResultValue8, ReturnType>
+): Pipeline.PipelineFunction<ValueType, ValueType>;
+
+export function tap<
+  ValueType,
+  ResultValue1,
+  ResultValue2,
+  ResultValue3,
+  ResultValue4,
+  ResultValue5,
+  ResultValue6,
+  ResultValue7,
+  ResultValue8,
+  ResultValue9,
+  ReturnType
+>(
+  f1: Pipeline.StepFunction<ValueType, ResultValue1>,
+  f2: Pipeline.StepFunction<ResultValue1, ResultValue2>,
+  f3: Pipeline.StepFunction<ResultValue2, ResultValue3>,
+  f4: Pipeline.StepFunction<ResultValue3, ResultValue4>,
+  f5: Pipeline.StepFunction<ResultValue4, ResultValue5>,
+  f6: Pipeline.StepFunction<ResultValue5, ResultValue6>,
+  f7: Pipeline.StepFunction<ResultValue6, ResultValue7>,
+  f8: Pipeline.StepFunction<ResultValue7, ResultValue8>,
+  f9: Pipeline.StepFunction<ResultValue8, ResultValue9>,
+  f10: Pipeline.StepFunction<ResultValue9, ReturnType>
+): Pipeline.PipelineFunction<ValueType, ValueType>;
 
 /**
- * Decorator that allows to transparently perform actions or side-effects,
- * such as logging and return the previous step result.
+ * Decorator that allows to transparently perform actions or side-effects
+ * without losing the initial value that triggered them.
  *
- * Tap supports promises, but it will ignore any error thrown inside the tapped
- * promise and will wait for its execution just to ignore its value.
+ * All the step functions inside the tap pass the value as in a pipeline, but
+ * the result of the tap will be the value of the previous step.
  *
- * The step inside tap does not need to return anything, since if it returns anything,
- * it will be ignored anyways in favour of what was in the previous step.
+ * If an error occurs in the flow inside the tap, the flow is interrupted and goes
+ * to the error handler. This is to support asynchronous validations inside tap.
  *
- * @param {Pipeline.StepFunction<TIn, any>} stepFn - The step fn to execute
+ * It is important to note that tap does not parallelize any execution of the promises
+ * inside the decorator. The execution will have to finish before the next step.
+ *
+ * @param {stepFunctions: Array<Pipeline.StepFunction<T, R>>} stepFunctions - The step functions to execute
  * @returns {Pipeline.StepFunction<TIn, TIn>}
  *
- * @example
- *
- * const { tap, step, pipe } require('@axa/bautajs-core');
- *
- * const randomPreviousPipeline = step(() => 'I am so random!');
- *
- *  const pipeline = pipe(
- *    randomPreviousPipeline,
- *    tap((prev) => {
- *      console.log(`some intermediate step. Prev is "${prev}""`);
- *      // prints 'some intermediate step. Prev is "I am so random!"'
- *    }),
- *    tap(async (prev) => {
- *      await asyncProcess();
- *      //'whether asyncProcess resolves or rejects. Prev still will be "I am so random!"'
- *    }),
- *    (prev) => {
- *      console.log(prev);
- *      // prints 'I am so random!'
- *    }
- *  );
  *
  */
-export function tap<TIn>(
-  stepFn: Pipeline.StepFunction<TIn, TIn | void>
-): Pipeline.StepFunction<TIn, TIn | void> {
-  return (prev, ctx, bautajs) => {
-    const result = stepFn(prev, ctx, bautajs);
 
-    if (isPromise(result)) {
-      return (result as Promise<typeof result>).then(() => prev).catch(() => prev);
+export function tap<T, R>(
+  ...stepFunctions: Array<Pipeline.StepFunction<T, R>>
+): Pipeline.StepFunction<T, T> {
+  if (stepFunctions.length === 0 || !stepFunctions.every(fn => typeof fn === 'function')) {
+    throw new Error('All tap inputs must be a function or a promise.');
+  }
+
+  // Merge all step functions in one composited step function
+  const composition: Pipeline.StepFunction<T, R> = stepFunctions.reduce(
+    (acc: Pipeline.StepFunction<T, R> | undefined, fn) => {
+      // First iteration will always return the first function since the default value is undefined
+      if (!acc) {
+        return fn;
+      }
+      // eslint-disable-next-line no-param-reassign
+      acc = compose(acc, fn);
+
+      return acc;
+    },
+    undefined
+  ) as Pipeline.StepFunction<T, R>;
+
+  let errorHandler: Pipeline.CatchError<GenericError> = defaultErrorHandler;
+
+  function tapStepFunction(prev: T, ctx: Context, bautaJS: BautaJSInstance): PromiseLike<T> | T {
+    try {
+      const result = composition(prev, ctx, bautaJS);
+
+      if (isPromise(result)) {
+        // If it is a promise we either return the value of the previous step or we interrup the tap flow if an error has occurred
+        return (result as Promise<R>)
+          .then(() => prev) // tap behaviour is ignoring the results of its steps and return the result value of the previous step
+          .catch((e: GenericError) => {
+            // This pattern is to ensure that when the error handler throws we throw, and if the error handler *does not* throw,
+            // tap behaviour is maintained and we return the result value of the previous step
+            errorHandler(e, ctx, bautaJS) as any;
+            return prev;
+          });
+      }
+
+      return prev;
+    } catch (e: any) {
+      // This pattern is to ensure that when the error handler throws we throw, and if the error handler *does not* throw,
+      // tap behaviour is maintained and we return the result value of the previous step
+      errorHandler(e, ctx, bautaJS) as any;
+      return prev;
     }
-    return prev;
+  }
+
+  tapStepFunction.catchError = function catchError(
+    fn: Pipeline.CatchError<any>
+  ): Pipeline.StepFunction<T, T> {
+    // Even when through casting we can bypass the typescript type check we do not allow non-functions as this parameter
+    if (typeof fn !== 'function') {
+      throw new Error('Tap catchError function must be called with a function or a promise.');
+    }
+    errorHandler = fn;
+
+    return tapStepFunction;
   };
+
+  return tapStepFunction;
 }
--- packages/bautajs-core/src/decorators/test/tap.test.ts
+++ packages/bautajs-core/src/decorators/test/tap.test.ts
@@ -53,7 +53,7 @@ describe('tap decorator', () => {
     expect(log).toHaveBeenCalledWith('star wars');
   });
 
-  test('should ignore the error inside tap and return the previous step value', async () => {
+  test('should throw the error inside tap', async () => {
     const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
     const tappedPromise = async (name: string) => {
@@ -68,8 +68,141 @@ describe('tap decorator', () => {
       tap(step<{ name: string }, { name: string }>(async ({ name }) => tappedPromise(name)))
     );
 
+    await expect(() => pipeline({}, createContext({}), {} as BautaJSInstance)).rejects.toThrow(
+      new Error('We have an error: star wars')
+    );
+  });
+
+  test('should work if we mix multiple sync and async steps', async () => {
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+    const log = jest.fn();
+    const tappedPromise = async (name: string) => {
+      await delay(100);
+      return log(name);
+    };
+
+    const tappedSyncFunction = (name: string) => `${name} is a great movie!`;
+
+    const getMovie = step(() => ({ name: 'star wars' }));
+
+    const pipeline = pipe(
+      getMovie,
+      tap(
+        step<{ name: string }, string>(({ name }) => name),
+        tappedSyncFunction,
+        tappedPromise
+      )
+    );
+
     await expect(pipeline({}, createContext({}), {} as BautaJSInstance)).resolves.toStrictEqual({
       name: 'star wars'
     });
+
+    expect(log).toHaveBeenCalledWith('star wars is a great movie!');
+  });
+
+  test('should work with a custom error handler', async () => {
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+    const getMovie = step(() => ({ name: 'star wars' }));
+
+    const log = jest.fn();
+
+    const asyncValidationPromise = async (name: string) => {
+      await delay(100);
+
+      if (name.length > 10) {
+        throw new Error(`Name ${name} is too long`);
+      }
+    };
+
+    const tappedSyncFunction = (name: string) => `${name} is a great movie!`;
+
+    const customErrorHandler = (e: Error) => {
+      log(e.message);
+      throw e;
+    };
+
+    const pipeline = pipe(
+      getMovie,
+      tap(
+        step<{ name: string }, string>(({ name }) => name),
+        tappedSyncFunction,
+        asyncValidationPromise
+      ).catchError(customErrorHandler)
+    );
+
+    await expect(pipeline({}, createContext({}), {} as BautaJSInstance)).rejects.toThrow(
+      new Error('Name star wars is a great movie! is too long')
+    );
+
+    expect(log).toHaveBeenCalledWith('Name star wars is a great movie! is too long');
+  });
+
+  test('should not allow an empty custom error handler', async () => {
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+    const getMovie = step(() => ({ name: 'star wars' }));
+
+    const asyncValidationPromise = async (name: string) => {
+      await delay(100);
+
+      if (name.length > 10) {
+        throw new Error(`Name ${name} is too long`);
+      }
+    };
+
+    const tappedSyncFunction = (name: string) => `${name} is a great movie!`;
+
+    const customErrorHandler = undefined;
+
+    expect(() =>
+      pipe(
+        getMovie,
+        tap(
+          step<{ name: string }, string>(({ name }) => name),
+          tappedSyncFunction,
+          asyncValidationPromise
+        ).catchError(customErrorHandler as any)
+      )
+    ).toThrow(new Error('Tap catchError function must be called with a function or a promise.'));
+  });
+
+  test('should return previous value when the custom error handler does not throw an error', async () => {
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+    const getMovie = step(() => ({ name: 'star wars' }));
+
+    const log = jest.fn();
+
+    const asyncValidationPromise = async (name: string) => {
+      await delay(100);
+
+      if (name.length > 10) {
+        throw new Error(`Name ${name} is too long`);
+      }
+    };
+
+    const tappedSyncFunction = (name: string) => `${name} is a great movie!`;
+
+    const customErrorHandler = (e: Error) => {
+      log(e.message);
+      return 'there is an error but we have decided to ignore it!';
+    };
+
+    const pipeline = pipe(
+      getMovie,
+      tap(
+        step<{ name: string }, string>(({ name }) => name),
+        tappedSyncFunction,
+        asyncValidationPromise
+      ).catchError(customErrorHandler)
+    );
+
+    await expect(pipeline({}, createContext({}), {} as BautaJSInstance)).resolves.toStrictEqual({
+      name: 'star wars'
+    });
+
+    expect(log).toHaveBeenCalledWith('Name star wars is a great movie! is too long');
   });
 });
