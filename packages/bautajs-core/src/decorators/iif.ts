a9f2d2c157377cb7d28814afea18d73bee2e48e8
Rodrigo Rivero <ro.rivero1991@gmail.com>
2024-05-08 12:08:36 +0200

    fix(core): fix iif return types (#152)

--- packages/bautajs-core/src/decorators/iif.ts
+++ packages/bautajs-core/src/decorators/iif.ts
@@ -1,4 +1,16 @@
 import { BautaJSInstance, Context, Pipeline } from '../types';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function iif<TIn, TPipelineOut, TElseNever>(
+  condition: (prev: TIn, ctx: Context, bautajs: BautaJSInstance) => boolean,
+  pipeline: Pipeline.StepFunction<TIn, TPipelineOut>
+): Pipeline.StepFunction<TIn, TIn | TPipelineOut>;
+
+export function iif<TIn, TPipelineOut, TElsePipelineOut>(
+  condition: (prev: TIn, ctx: Context, bautajs: BautaJSInstance) => boolean,
+  pipeline: Pipeline.StepFunction<TIn, TPipelineOut>,
+  elsePipeline: Pipeline.StepFunction<TIn, TElsePipelineOut>
+): Pipeline.StepFunction<TIn, TPipelineOut | TElsePipelineOut>;
+
 /**
  * Decorator that allows to execute given pipeline conditionally. It accepts the condition step function and pipeline.
  * If the condition step function will be evaluated to truthy value the pipeline will be executed.
